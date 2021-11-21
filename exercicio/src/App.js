b7fe49fe33eef58bcbb230f328b6dbf147246b82
Geliel1 <85584428+Geliel1@users.noreply.github.com>
2021-11-21 12:10:43 -0300

    finalizando tela de cadastro

---
+++ exercicio/src/App.js
@@ -0,0 +1,14 @@
+import Cadastro from './componentes/Cadastro';
+import React from './componentes/React';
+import './App.css';
+
+function App() {
+  return (
+    <div id="container">
+      <Cadastro />
+      <React />
+    </div>
+  );
+}
+
+export default App;
